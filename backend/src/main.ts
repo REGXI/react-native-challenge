44f5cad110a4be2dc5858b4e48d89e9504a80e79
JacksonRegxi <jackson@regxi.com>
2024-06-26 12:56:03 -0600

    chore: enable cors and allow all origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,37 +1,42 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import {NestFactory} from '@nestjs/core'
+import {AppModule} from './app.module'
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
 import {ResponseInterceptor} from "./shared/response.interceptor";
 import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  const config = new DocumentBuilder()
-    .setTitle('Challenge API')
-    .setDescription('Challenge API Doc')
-    .setVersion('1.0')
-    .addBearerAuth(
-      {
-        description: 'Default JWT Authorization',
-        type: 'http',
-        in: 'header',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-      },
-      'access-token',
-    )
-    .addTag('Challenge')
-    .build()
+    const app = await NestFactory.create(AppModule, { cors: true })
+    const config = new DocumentBuilder()
+        .setTitle('Challenge API')
+        .setDescription('Challenge API Doc')
+        .setVersion('1.0')
+        .addBearerAuth(
+            {
+                description: 'Default JWT Authorization',
+                type: 'http',
+                in: 'header',
+                scheme: 'bearer',
+                bearerFormat: 'JWT',
+            },
+            'access-token',
+        )
+        .addTag('Challenge')
+        .build()
+    app.enableCors({
+        origin: ['*'],
+        methods: ['*'],
+        credentials: true
+    })
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('api/docs', app, document, {
+        swaggerOptions: {
+            persistAuthorization: true,
+        },
+    })
 
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api/docs', app, document, {
-    swaggerOptions: {
-      persistAuthorization: true,
-    },
-  })
-
-  app.useGlobalInterceptors(new ResponseInterceptor())
-  app.useGlobalPipes(new ValidationPipe())
-  await app.listen(3000)
+    app.useGlobalInterceptors(new ResponseInterceptor())
+    app.useGlobalPipes(new ValidationPipe())
+    await app.listen(3000)
 }
+
 bootstrap()
